73ddf19811d9c7bb316ffb39f99e7d44df16aca9
Svitkov Sergey <sum6969@yandex.ru>
2016-10-19 19:28:48 +0300

    Works, but not correct yet

--- src/main/CountingAction.java
+++ src/main/CountingAction.java
@@ -1,31 +1,25 @@
 package main;
 
-import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
-import com.intellij.openapi.editor.actionSystem.EditorActionManager;
-import com.intellij.openapi.editor.actionSystem.TypedAction;
-import com.intellij.openapi.externalSystem.model.Key;
-import com.intellij.openapi.project.Project;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.wm.KeyEventProcessor;
-import org.jetbrains.annotations.NotNull;
-
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 public class CountingAction extends AnAction {
-    private final KeyPressedHandler keyPressedHandler = new KeyPressedHandler();
-    private final EditorActionManager actionManager = EditorActionManager.getInstance();
-    private final TypedAction typedAction = actionManager.getTypedAction();
+
+    private CountingListener countingListener;
 
     public CountingAction() {
         super("Counting");
-        typedAction.setupHandler(keyPressedHandler);
+        countingListener = new CountingListener();
     }
 
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
-        Messages.showInfoMessage("Symbols typed: " + keyPressedHandler.getSymbolsTyped(), "Symbols typed");
+        Document document = anActionEvent.getRequiredData(CommonDataKeys.EDITOR).getDocument();
+        document.addDocumentListener(countingListener);
+        Messages.showInfoMessage("Symbols typed: " + countingListener.getSymbolsCount()
+                + "\nTotal symbols count: " + document.getTextLength(), "Symbols typed");
     }
 }
